77d5b745972215322bf893ab3cf8b34262b14b66
empirefox <empirefoxit@sina.com>
2015-06-14 20:22:06 +0800

    use gulp-swig in copy:html

--- gulp/tasks/copy/html.js
+++ gulp/tasks/copy/html.js
@@ -3,37 +3,47 @@ var plugins = require('gulp-load-plugins')();
 var cdnjsOps = require('./cdnjsOps.json');
 var cdnHelper = require('./cdn-helper');
 var config = require('../../config');
+var swig = plugins.swig;
 
 function addCopyPageTask(pagename) {
 	var resource = require('../../resource/' + pagename + '.json');
 
 	gulp.task('copy:' + pagename + '.html', function() {
-		return gulp.src('./src/' + pagename + '.html')
+		return gulp.src('./src/' + pagename + '.html').
+		// replace module with pagename
+		pipe(swig({
+			defaults : {
+				varControls : ['[[', ']]']
+			},
+			data : {
+				module : pagename
+			}
+		})).
 		// add jsdelivr css
-		.pipe(cdnHelper.toJsdelivrCss(resource.jsdelivr.css))
+		pipe(cdnHelper.toJsdelivrCss(resource.jsdelivr.css)).
 		// add jsdelivr js
-		.pipe(cdnHelper.toJsdelivrJs(resource.jsdelivr.js))
+		pipe(cdnHelper.toJsdelivrJs(resource.jsdelivr.js)).
 		// inject css files to html
-		.pipe(plugins.inject(gulp.src(resource.cdn.css, {
+		pipe(plugins.inject(gulp.src(resource.cdn.css, {
 			read : false
 		}), {
 			name : 'head'
-		}))
+		})).
 		// inject js files to html
-		.pipe(plugins.inject(gulp.src(resource.cdn.js, {
+		pipe(plugins.inject(gulp.src(resource.cdn.js, {
 			read : false
 		}), {
 			name : 'head'
-		}))
+		})).
 		// replace local file to cdn url
-		.pipe(plugins.cdnizer({
+		pipe(plugins.cdnizer({
 			fallbackTest : null,
 			files : cdnjsOps
-		}))
+		})).
 		// replace with Staticfiles cdn
-		.pipe(cdnHelper.toStaticfilesCDN())
+		pipe(cdnHelper.toStaticfilesCDN()).
 		// output
-		.pipe(gulp.dest(config.dest));
+		pipe(gulp.dest(config.dest));
 	});
 }
 
