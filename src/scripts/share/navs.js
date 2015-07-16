6d0b7a92d9721e9ca9cf5ec7122b047da5ac8f3f
empirefox <empirefoxit@sina.com>
2015-07-16 16:37:29 +0800

    fix navbar

--- src/scripts/share/navs.js
+++ src/scripts/share/navs.js
@@ -1,35 +1,25 @@
 'use strict';
 
 angular.module('app.navs', ['ngDialog', 'app.i18n', 'app.service.login']).directive('navBar', [
-  '$translate', 'Navs',
-  function($translate, Navs) {
+  '$translate', 'NavsService',
+  function($translate, NavsService) {
     return {
       restrict: 'E',
       templateUrl: '/views/share/navbar.html',
       link: function(scope) {
-        scope.navs = Navs;
+        NavsService.get().then(function(navs) {
+          scope.navs = navs;
+        });
         scope.change = $translate.use;
       },
     };
   }
-]).factory('Navs', [
-  '$window', '$http', 'ngDialog', 'NavList', 'LoginChecker',
-  function($window, $http, ngDialog, NavList, LoginChecker) {
+]).factory('NavsService', [
+  '$window', '$http', '$q', 'ngDialog', 'NavList', 'LoginChecker',
+  function($window, $http, $q, ngDialog, NavList, LoginChecker) {
+    var path = window.location.pathname;
 
-    function confirm(nav) {
-      if (nav.dialog) {
-        ngDialog.open({
-          template: '/views/share/dialogs/' + nav.dialog + '.html',
-          className: 'ngdialog-theme-plain',
-          showClose: true,
-        }).then(function() {
-          $window.location.assign(nav.href);
-        });
-      }
-    }
-
-    return NavList.map(function(nav) {
-      var path = window.location.pathname;
+    function initNav(nav) {
       nav.visible = nav.hideFrom.every(function(hide) {
         if (path !== hide) {
           return true;
@@ -38,7 +28,7 @@ angular.module('app.navs', ['ngDialog', 'app.i18n', 'app.service.login']).direct
       nav.active = nav.href === path;
 
       nav.go = function() {
-        if (nav.needAuth) {
+        if (nav.authOnly) {
           LoginChecker.check().
           then(function() {
             confirm(nav);
@@ -50,14 +40,61 @@ angular.module('app.navs', ['ngDialog', 'app.i18n', 'app.service.login']).direct
         }
       };
       return nav;
+    }
+
+    function seperate(needSeperate) {
+      if (needSeperate) {
+        return LoginChecker.check().then(function() {
+          return true;
+        }, function() {
+          return false;
+        }).then(function(authed) {
+          return NavList.filter(function(nav) {
+            return !((nav.authOnly && !authed) || (nav.unauthedOnly && authed));
+          });
+        });
+      } else {
+        return NavList;
+      }
+    }
+
+    function confirm(nav) {
+      if (nav.dialog) {
+        ngDialog.open({
+          template: '/views/share/dialogs/' + nav.dialog + '.html',
+          className: 'ngdialog-theme-plain',
+          showClose: true,
+        }).then(function() {
+          $window.location.assign(nav.href);
+        });
+      } else {
+        $window.location.assign(nav.href);
+      }
+    }
+
+    // include path === '/' || path === 'index.html', cause no authed provided
+    var self = {};
+    NavList.some(function(nav) {
+      if (nav.href === path) {
+        self = nav;
+        return true;
+      }
     });
+
+    return {
+      get: function() {
+        return $q.when(!self.authOnly && !self.unauthedOnly).then(seperate).then(function(navs) {
+          return navs.map(initNav);
+        });
+      },
+    };
   }
 ]).constant('NavList', [{
   href: '/rooms.html',
   icon: 'fa fa-desktop',
   txt: 'PAGE_NAME.ROOMS',
   hideFrom: ['/login.html'],
-  needAuth: true,
+  authOnly: true,
   right: false,
   hideXs: false,
 }, {
@@ -65,7 +102,7 @@ angular.module('app.navs', ['ngDialog', 'app.i18n', 'app.service.login']).direct
   icon: 'fa fa-plus',
   txt: 'PAGE_NAME.REG_ROOM',
   hideFrom: ['/login.html'],
-  needAuth: true,
+  authOnly: true,
   right: true,
   hideXs: true,
 }, {
@@ -73,7 +110,7 @@ angular.module('app.navs', ['ngDialog', 'app.i18n', 'app.service.login']).direct
   icon: 'fa fa-wifi',
   txt: 'PAGE_NAME.JOIN',
   hideFrom: ['/login.html'],
-  needAuth: true,
+  authOnly: true,
   right: false,
   hideXs: false,
 }, {
@@ -81,7 +118,7 @@ angular.module('app.navs', ['ngDialog', 'app.i18n', 'app.service.login']).direct
   icon: 'fa fa-user-times',
   txt: 'PAGE_NAME.LOGOFF',
   hideFrom: ['/login.html'],
-  needAuth: true,
+  authOnly: true,
   right: false,
   dailog: 'ConfirmLogoff',
   hideXs: true,
@@ -90,15 +127,16 @@ angular.module('app.navs', ['ngDialog', 'app.i18n', 'app.service.login']).direct
   icon: 'fa fa-bolt',
   txt: 'PAGE_NAME.LOGOUT',
   hideFrom: ['/login.html'],
-  needAuth: true,
+  authOnly: true,
   right: false,
   hideXs: false,
 }, {
   href: '/login.html',
-  icon: 'fa fa-wifi',
-  txt: 'PAGE_NAME.JOIN',
-  hideFrom: ['/', '/rooms.html', '/reg-room.html', '/join.html', '/login.html'],
-  needAuth: false,
+  icon: 'fa fa-sign-in',
+  txt: 'PAGE_NAME.LOGIN',
+  hideFrom: ['/rooms.html', '/reg-room.html', '/join.html', '/login.html'],
+  authOnly: false,
   right: false,
   hideXs: false,
+  unauthedOnly: true,
 }]);
--- src/scripts/share/translate.js
+++ src/scripts/share/translate.js
@@ -1,6 +1,8 @@
 'use strict';
 
-angular.module('app.i18n', ['app.i18n.en_US', 'app.i18n.zh_CN']).config(['$translateProvider', function(
+angular.module('app.i18n', ['ngCookies', 'app.i18n.en_US', 'app.i18n.zh_CN']).config(['$translateProvider', function(
   $translateProvider) {
   $translateProvider.determinePreferredLanguage();
+  $translateProvider.fallbackLanguage('en_US');
+  $translateProvider.useCookieStorage();
 }]);
