66836a4d864458b8e6a13afd73570b83b7288988
empirefox <empirefoxit@sina.com>
2015-12-31 16:25:53 +0800

    publish client1.8.0

--- src/scripts/services/client-dl-factory.js
+++ src/scripts/services/client-dl-factory.js
@@ -3,22 +3,22 @@
 angular.module('dl.client.data', []).factory('ClientData', ['$q', function($q) {
   var data = [{
     icon: 'ic-client-50x60.png',
-    size: '39.81MB',
+    size: '40MB',
     url: {
       // bit32: 'http://32',
-      bit64: 'https://dn-icv3r1.qbox.me/dl/ic-client_1.7.0_amd64.deb',
+      bit64: 'https://dn-icv3r1.qbox.me/dl/ic-client_1.8.0_amd64.deb',
     },
     os: 'Linux',
     arch: '64bit',
-    time: 1450939738879,
+    time: 1451550141612,
     'en_US': {
       name: 'IC Room',
-      version: '1.7.0',
+      version: '1.8.0',
       desc: 'This is the client software for ic center of gogogocam.com.',
     },
     'zh_CN': {
       name: 'IC监控室',
-      version: '1.7.0',
+      version: '1.8.0',
       desc: '这是监控中心的客户端，运行在与IP摄像头相同的内网中. 运行后，您将可以安全远程查看内网监控. 您的家人可以参与到监控室.',
     },
   }];
